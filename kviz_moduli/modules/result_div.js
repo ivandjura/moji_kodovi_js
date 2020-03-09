3572da3ec7673ceb103c0e9ef34a90aea6f276a1
ivandjura <djuraivan@gmail.com>
2020-03-09 17:05:18 +0100

    ponedeljak posle testa moduli i modifikacija kviza

---
+++ kviz_moduli/modules/result_div.js
@@ -0,0 +1,15 @@
+//module result_div.js
+let createDiv = (cls, parent, whereChild) => {
+  let div = document.createElement("div");
+  div.classList.add(cls);
+  let text = document.createElement("div");
+  text.classList.add("text");
+  let h2 = document.createElement("h2");
+  text.appendChild(h2);
+  div.appendChild(text);
+  parent.insertBefore(div, whereChild);
+  div.style.display = "none";
+  return [div, h2];
+};
+
+export default createDiv;
