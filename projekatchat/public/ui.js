bd67f2dd49a4369c6c9bf4680902a37be05c38bc
ivandjura <djuraivan@gmail.com>
2020-03-25 17:09:35 +0100

    ispravljena greska ui jelenin kod

--- projekatchat/public/ui.js
+++ projekatchat/public/ui.js
@@ -3,9 +3,12 @@ export class ChatUI {
   constructor(l) {
     this.list = l;
   }
+
+  //geter i seter
   set list(l) {
     this._list = l;
   }
+
   get list() {
     return this._list;
   }
@@ -17,35 +20,34 @@ export class ChatUI {
     let h = date.getHours();
     let min = date.getMinutes();
 
-    //dodavanje 0 ispred jednocifrenih vrednosti
+    //Dodavanje 0 ispred jednocifrenih vrednosti
     d = String(d).padStart(2, "0");
     m = String(m).padStart(2, "0");
     h = String(h).padStart(2, "0");
-    min = String(h).padStart(2, "0");
+    min = String(min).padStart(2, "0");
 
-    let strDate = d + "." + m + "." + "." + y + ". -" + h + ":" + min;
+    let strDate = d + "." + m + "." + y + ". - " + h + ":" + min;
 
     return strDate;
   }
 
-  //metod koji pravi osnovu za prikaz list itema
-  //data koji prosledjujem je objekat tj. dokument iz baze
+  //Metod koji pravi osnovu za prikaz list item-a
+  //data koji prosleđujemo je objekat tj. dokument iz baze
   templateLI(data) {
     let date = data.created_at.toDate();
     let strDate = this.formatDate(date);
 
     let htmlLI = `<li `;
-    if (data.username == localStorage.usernameLs) {
+    if (data.username == localStorage.usernameLS) {
       htmlLI += `class="me">`;
     } else {
-      htmlLI += `class="not-me"`;
+      htmlLI += `class="not-me">`;
     }
 
-    htmlLI += `<li>
-        <span class="username">${data.username} : </span>
-        <span class="message">${data.message}</span>
-        <div class="date">${strDate}</div> 
-    </li>`;
+    htmlLI += `   <span class="username">${data.username} : </span>
+          <span class="message">${data.message}</span>
+          <div class="date">${strDate}</div>
+      </li>`;
     this.list.innerHTML += htmlLI;
     this.list.scrollTop = this.list.scrollHeight;
   }
